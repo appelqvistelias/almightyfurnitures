9e547ce37bdd9b18bf5812bb2b79977f98727265
Luisal182 <luisal.arranz@hotmail.com>
2025-03-03 12:46:32 +0100

    resolve conflict web 3

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,27 @@
 <?php
 
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Controllers\Admin\ProductController as AdminProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 
 Route::view('/', 'home');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::middleware([AdminMiddleware::class])->prefix('admin')->group(function () {
+        Route::get('/products', [AdminProductController::class, 'index'])->name('admin.products.index');
+    });
+});
 
 Route::resource('categories', CategoryController::class);
 
@@ -12,18 +29,13 @@ Route::controller(ProductController::class)
     ->prefix('products')
     ->name('products.')
     ->group(function () {
-
         Route::get('/', 'index')->name('index');
-
         Route::get('/create', 'create')->name('create');
-
         Route::post('/store', 'store')->name('store');
-
         Route::get('/{product}', 'show')->name('show');
-
         Route::get('/{product}/edit', 'edit')->name('edit');
-
         Route::patch('/{product}', 'update')->name('update');
-
         Route::delete('/{product}', 'destroy')->name('destroy');
     });
+
+require __DIR__ . '/auth.php';
