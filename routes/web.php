c03887e7544c031758b17a774fc799d1b91c843e
Luisal182 <luisal.arranz@hotmail.com>
2025-03-03 12:26:07 +0100

    resolve conflict 2

--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,29 @@
 <?php
 
-use App\Http\Middleware\AdminMiddleware;
-use App\Http\Controllers\Admin\ProductController as AdminProductController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 
 Route::view('/', 'home');
 
-// Categories routes
 Route::resource('categories', CategoryController::class);
 
-// Product routes
 Route::controller(ProductController::class)
     ->prefix('products')
     ->name('products.')
     ->group(function () {
+
         Route::get('/', 'index')->name('index');
+
         Route::get('/create', 'create')->name('create');
+
         Route::post('/store', 'store')->name('store');
+
         Route::get('/{product}', 'show')->name('show');
+
         Route::get('/{product}/edit', 'edit')->name('edit');
+
         Route::patch('/{product}', 'update')->name('update');
+
         Route::delete('/{product}', 'destroy')->name('destroy');
     });
-
-// Dashboard and authentication routes
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    // Admin routes
-    Route::middleware([AdminMiddleware::class])->prefix('admin')->group(function () {
-        Route::get('/products', [AdminProductController::class, 'index'])->name('admin.products.index');
-    });
-});
-
-require __DIR__ . '/auth.php';
