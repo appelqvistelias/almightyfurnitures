fabfc3755d8e0714b55b6ef9b51ab202a0bce1c9
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-03-06 18:28:10 +0100

    made text slightly bigger

--- resources/views/components/errors.blade.php
+++ resources/views/components/errors.blade.php
@@ -2,7 +2,7 @@
 <div class="space-y-4">
     @foreach ($errors->all() as $error)
     <div class="bg-red-500 text-white p-4 rounded-lg">
-        <p class="text-sm">{{ $error }}</p>
+        <p class="text-base">{{ $error }}</p>
     </div>
     @endforeach
 </div>
