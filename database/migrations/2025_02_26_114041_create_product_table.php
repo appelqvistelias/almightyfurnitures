52ace8feb83274e15c21b6708403658b9d189de5
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-03-08 18:04:42 +0100

    changed to products

--- database/migrations/2025_02_26_114041_create_product_table.php
+++ database/migrations/2025_02_26_114041_create_product_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name', length: 100);
-            $table->string('image')->nullable(); ///-----new for images.
+            $table->string('image')->nullable();
             $table->text('description')->nullable();
             $table->integer('height')->nullable();
             $table->integer('width')->nullable();
@@ -34,6 +34,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('product');
+        Schema::dropIfExists('products');
     }
 };
