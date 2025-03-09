e712dc5ebd6842ea9544e07c95dd4a4cbf2329c5
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-03-09 10:17:25 +0100

    tweaked button styling

--- resources/views/components/products/edit-form.blade.php
+++ resources/views/components/products/edit-form.blade.php
@@ -81,11 +81,12 @@
 
     <!-- Save and Cancel Button -->
     <div class="mt-4 flex justify-end space-x-4">
-        <a href="{{ route('products.index') }}" class="px-4 py-2 bg-gray-300 text-gray-700 rounded-lg hover:bg-gray-400">
+        <a href="{{ route('products.index') }}" class="px-6 py-3 bg-gray-300 text-gray-700 rounded-lg hover:bg-gray-400 flex items-center justify-center">
             Cancel
         </a>
-        <button type="submit" class="px-6 py-3 bg-green-500 text-white font-semibold rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500">
+        <button type="submit" class="px-6 py-3 bg-green-500 text-white font-semibold rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 flex items-center justify-center">
             Save
         </button>
     </div>
+
 </form>
