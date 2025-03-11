3bcc7441d49d1fb9a0faf759a484acd74c378a85
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-03-11 13:07:07 +0100

    removed comment

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ Route::get('/product-details/{id}', [HomeController::class, 'showProductDetails'
 
 // Authenticated routes
 Route::middleware('auth')->group(function () {
-    // Profilhantering
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
